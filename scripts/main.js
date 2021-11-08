d44a8e3777dd33e6acdd7a3eb685d67be81ade7d
root <root@LAPTOP-42UKCJM1.localdomain>
2021-11-08 23:38:19 +0200

    Added grid logic for automatic fitting over a box

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
-let container = document.getElementById('framecontainer');
-const amountPixels = 256;
+// fix containers size, so it doesnt move
+// create a hover item so it changes color on hover
+// create some sort of choice mechanism with YxY choices, so user can choose
 
+const container = document.getElementById("framecontainer");
+const amountPixels = 64;
 
-for(i = 1; i <= amountPixels; i++) {
-  let newElement = document.createElement('div');
-  newElement.textContent = i;
+for (i = 1; i <= amountPixels; i++) {
+  let newElement = document.createElement("div");
+  newElement.setAttribute("class", "gridbox");
   container.appendChild(newElement);
+  newElement.innerHTML = i;
 }
 
+let selection = document.querySelectorAll(".gridbox");
+for (i = 0; i <= selection.length; i++) {
+  selection[i].style.width = 100 / Math.sqrt(amountPixels) + "%";
+  selection[i].style.height = 100 / Math.sqrt(amountPixels) + "%";
+}
