bc2f1f1871d1c141a1221ccec19b7df2b637c223
vilas <viliusrasickis@gmail.com>
2021-11-11 16:40:39 +0200

    Final version (..for now). There is still a lot of spaghetti code, but thickness functionality works, which is not common for this project around other students.

--- scripts/main.js
+++ scripts/main.js
@@ -22,8 +22,8 @@ const updateAttributes = (element, attributes) => {
 updateAttributes(thickness, {
   type: "range",
   min: "1",
-  max: "100",
-  value: "50",
+  max: "3",
+  value: "2",
   id: "thicknessRange",
 });
 thicknessContainer.appendChild(thickness);
@@ -38,42 +38,198 @@ valueSlider.appendChild(spanSlider);
 
 const thicknessRange = document.getElementById("thicknessRange");
 const spanSliderGet = document.getElementById("span");
-spanSliderGet.innerHTML = thicknessRange.value;
+spanSliderGet.innerHTML = "Medium";
 
 thicknessRange.oninput = function () {
-  spanSliderGet.innerHTML = this.value;
+  if(this.value === "1") {
+    spanSliderGet.innerHTML = "Minimum";
+  } else if (this.value === "2") {
+    spanSliderGet.innerHTML = "Medium";
+  } else {
+    spanSliderGet.innerHTML = "Maximum";
+  }
 };
 
-const amountPixels = 4096;
 const container = document.getElementById("frameContainer");
 
-for (i = 1; i <= amountPixels; i++) {
-  let newElement = document.createElement("div");
-  newElement.setAttribute("id", "gridbox" + i);
-  newElement.setAttribute("class", "gridbox");
-  container.appendChild(newElement);
+const horizontalX = 100;
+const verticalY = horizontalX;
+const totalPx = horizontalX * verticalY;
+
+for (x = 1; x <= horizontalX; x++) {
+  for(y = 1; y <= verticalY; y++) {
+    let newElement = document.createElement("div");
+    newElement.setAttribute("class", "gridbox");  
+    newElement.setAttribute("id", x + "-" + y);
+    container.appendChild(newElement);
+  }
 }
 
 let selection = document.querySelectorAll(".gridbox");
 for (i = 0; i < selection.length; i++) {
-  selection[i].style.width = 100 / Math.sqrt(amountPixels) + "%";
-  selection[i].style.height = 100 / Math.sqrt(amountPixels) + "%";
+  selection[i].style.width = 100 / Math.sqrt(totalPx) + "%";
+  selection[i].style.height = 100 / Math.sqrt(totalPx) + "%";
 }
 
 let targetElement;
-for (i = 1; i <= amountPixels; i++) {
-  targetElement = document.getElementById("gridbox" + i);
-  targetElement.addEventListener("mouseover", changeColor);
+for (x = 1; x <= horizontalX; x++) {
+  for(y = 1; y <= verticalY; y++) {
+    targetElement = document.getElementById(x + "-" + y);
+    targetElement.addEventListener("mouseover", changeColor);
+  }
 }
 
-function changeColor(event) {
+function changeColor (event) {
   newElement = event.target.id;
-  document.getElementById(newElement).style.background = "#646464";
-}
+  const xCoord = parseInt(newElement.split('-')[0]);
+  const yCoord = parseInt(newElement.split('-')[1]);
 
-/* Another way to get the color on the div
-let targetElement;
-for (i = 1; i <= amountPixels; i++) {
-  targetElement = document.getElementById("gridbox" + i);
-  targetElement.setAttribute("onmouseover", "this.style.background = 'blue'");
-}*/
+  if(spanSliderGet.innerHTML === "Minimum") {
+    document.getElementById(newElement).style.background = "#646464";
+  }
+
+  if (spanSliderGet.innerHTML === "Medium") {
+    let xOneMore;
+    let xOneLess;
+    let yOneMore;
+    let yOneLess
+    if (xCoord === 12) {
+      xOneMore = 12;
+    } else {
+      xOneMore = xCoord + 1;
+    }
+
+    if (xCoord === 1) {
+      xOneLess = 1;
+    } else {
+      xOneLess = xCoord - 1;
+    }
+
+    if (yCoord === 12) {
+      yOneMore = 12;
+    } else {
+      yOneMore = yCoord + 1;
+    }
+
+    if (yCoord === 1) {
+      yOneLess = 1;
+    } else {
+      yOneLess = yCoord - 1;
+    }
+ 
+    topLeft = (xOneLess) + "-" + (yOneLess);
+    document.getElementById(topLeft).style.background = "#646464"; 
+    topTop = (xOneLess) + "-" + (yCoord);
+    document.getElementById(topTop).style.background = "#646464";
+    topRight = (xOneLess) + "-" + (yOneMore);
+    document.getElementById(topRight).style.background = "#646464"; 
+    midLeft = xCoord + "-" + (yOneLess);
+    document.getElementById(midLeft).style.background = "#646464";
+    midMid = xCoord + "-" + yCoord; 
+    document.getElementById(midMid).style.background = "#646464";
+    midRight = xCoord + "-" + (yOneMore); 
+    document.getElementById(midRight).style.background = "#646464";
+    bottomLeft = (xOneMore) + "-" + (yOneLess); 
+    document.getElementById(bottomLeft).style.background = "#646464";
+    bottomBottom = (xOneMore) + "-" + (yCoord); 
+    document.getElementById(bottomBottom).style.background = "#646464";
+    bottomRight = (xOneMore) + "-" + (yOneMore); 
+    document.getElementById(bottomRight).style.background = "#646464";
+  }
+
+  if(spanSliderGet.innerHTML === "Maximum") {
+    let xOneMore;
+    let xTwoMore;
+    let xOneLess;
+    let xTwoLess;
+    let yOneMore;
+    let yTwoMore;
+    let yOneLess;
+    let yTwoLess;
+
+    if (xCoord === horizontalX) {
+      xOneMore = horizontalX;
+    } else {
+      xOneMore = xCoord + 1;
+      xTwoMore = xCoord + 2;
+    }
+
+    if (xCoord === 1) {
+      xOneLess = 1;
+    } else {
+      xOneLess = xCoord - 1;
+      xTwoLess = xCoord - 2;
+    }
+
+    if (yCoord === verticalY) {
+      yOneMore = verticalY;
+    } else {
+      yOneMore = yCoord + 1;
+      yTwoMore = yCoord + 2;
+    }
+
+    if (yCoord === 1) {
+      yOneLess = 1;
+    } else {
+      yOneLess = yCoord - 1;
+      yTwoLess = yCoord - 2;
+    }
+ 
+    topLeft = xOneLess + "-" + yOneLess;
+    document.getElementById(topLeft).style.background = "#646464"; 
+    topTopLeftLeft = xTwoLess + "-" + yTwoLess;
+    document.getElementById(topTopLeftLeft).style.background = "#646464"; 
+    topTopLeft = xOneLess + "-" + yTwoLess;
+    document.getElementById(topTopLeft).style.background = "#646464"; 
+
+    topTop = xCoord + "-" + yOneLess;
+    document.getElementById(topTop).style.background = "#646464";
+    topTopTop = xCoord + "-" + yTwoLess;
+    document.getElementById(topTopTop).style.background = "#646464";
+
+    topRight = xOneLess + "-" + yOneMore;
+    document.getElementById(topRight).style.background = "#646464";
+    topTopRight = xOneMore + "-" + yTwoMore;
+    document.getElementById(topTopRight).style.background = "#646464";
+    topTopRightRight = xTwoMore + "-" + yTwoMore;
+    document.getElementById(topTopRightRight).style.background = "#646464";
+    
+    midLeft = xCoord + "-" + yOneLess;
+    document.getElementById(midLeft).style.background = "#646464";
+    midLeftTop = xTwoLess + "-" + yOneLess;
+    document.getElementById(midLeftTop).style.background = "#646464";
+    midLeftLeft = xTwoLess + "-" + yCoord;
+    document.getElementById(midLeftLeft).style.background = "#646464";
+    midLeftBottom = xTwoLess + "-" + yOneMore;
+    document.getElementById(midLeftBottom).style.background = "#646464";
+    
+    midMid = xCoord + "-" + yCoord; 
+    document.getElementById(midMid).style.background = "#646464";
+    
+    midRight = xCoord + "-" + yOneMore; 
+    document.getElementById(midRight).style.background = "#646464";
+    midRightTop = xTwoMore + "-" + yOneLess;
+    document.getElementById(midRightTop).style.background = "#646464";
+    midRightRight = xTwoMore + "-" + yCoord;
+    document.getElementById(midRightRight).style.background = "#646464";
+    midRightBottom = xTwoMore + "-" + yOneMore;
+    document.getElementById(midRightBottom).style.background = "#646464";
+    
+    bottomLeft = xOneMore + "-" + yOneLess; 
+    document.getElementById(bottomLeft).style.background = "#646464";
+    bottomBottomLeftLeft = xTwoLess+ "-" + yTwoMore; 
+    document.getElementById(bottomBottomLeftLeft).style.background = "#646464";
+    bottomBottomLeft= xOneLess+ "-" + yTwoMore; 
+    document.getElementById(bottomBottomLeft).style.background = "#646464";
+    bottomBottom = xOneMore + "-" + yCoord; 
+    document.getElementById(bottomBottom).style.background = "#646464";
+    bottomBottomBottom = xTwoMore + "-" + yCoord; 
+    document.getElementById(bottomBottomBottom).style.background = "#646464";
+    bottomRight = xOneMore + "-" + yOneMore; 
+    document.getElementById(bottomRight).style.background = "#646464";
+    bottomBottomRight = xOneMore + "-" + yTwoMore; 
+    document.getElementById(bottomBottomRight).style.background = "#646464";
+    bottomBottomRightRight = xTwoMore + "-" + yTwoMore; 
+    document.getElementById(bottomBottomRightRight).style.background = "#646464"
+  }
+}
